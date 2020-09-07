9b74fb663cf13b7f0728f4f02be11fb13e082f42
T Y <>
2020-09-07 12:40:33 +0900

    記事詳細ページへリンク追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,9 @@
 class ArticlesController < ApplicationController
     def index
-        
-        @articles = Article.all
+       @articles = Article.all
     end
 
-    
+    def show
+       @article = Article.find(params[:id])
+    end
 end
