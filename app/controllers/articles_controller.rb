d7040e5811f66003ff07db151a20c959f1e3e606
T Y <>
2020-09-17 00:57:52 +0900

    ホームを部分テンプレートに変更

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,17 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update]
+
     def index
        @articles = Article.all
     end
 
     def show
-       @article = Article.find(params[:id])
     end
 
     def new
         @article = Article.new
     end
-   
+
     def create
         @article =Article.new(article_params)
        if  @article.save
@@ -22,11 +23,9 @@ class ArticlesController < ApplicationController
     end
 
 def edit
-    @article = Article.find (params[:id])
 end
 
 def update
-    @article = Article.find (params[:id])
     if @article.update(article_params)
       redirect_to article_path(@article),notice: '更新できました'
     else
@@ -44,7 +43,11 @@ end
 
     private
     def article_params
-       
         params.require(:article).permit(:title, :content)
     end
+
+    def set_article
+        @article = Article.find (params[:id])
     end
+    end
+
