fd96c723d5cdfcbefb69e7957d174658f1b0e96d
T Y <>
2020-09-27 20:08:39 +0900

    likeテーブル作成

--- app/models/article.rb
+++ app/models/article.rb
@@ -26,6 +26,7 @@ class Article < ApplicationRecord
     validate :validate_title_and_content_length
 
    has_many :comments ,dependent: :destroy
+   has_many :likes, dependent: :destroy
     belongs_to :user
 
     def display_create_at
