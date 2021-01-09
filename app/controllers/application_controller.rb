9e6adffcfa96cb464c28d6d7f690c7a2a0b879df
T Y <>
2021-01-10 01:25:49 +0900

    フォロー機能とタイムライン実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
 
+    def current_user
+        ActiveDecorator::Decorator.instance.decorate(super) if super.present?
+        super
+    end
+
 end
